063416248d7dc11288e8ede9d3d84067f45019b6
Rafael Silverio <rafael.silverio.s@outlook.com>
2024-02-12 17:42:16 -0300

    React Router

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeContatos from '../../containers/ListaDeContatos'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeContatos />
+  </>
+)
+
+export default Home
